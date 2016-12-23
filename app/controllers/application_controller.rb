1101e2011f0e9aa9338145b188d655670a73bed5
Doug Black <dougblackjr@gmail.com>
2016-12-23 18:14:49 -0500

    Added public and private layouts
    For introduction to service

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+
+	protect_from_forgery with: :exception
+
+	layout :determine_layout
+
+	private
+
+		def determine_layout
+			current_user ? "application" : "intro"
+		end
+
 end
