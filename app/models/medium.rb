aa9e836c86f5be163de9626e599d27b6f466a666
Doug Black <dougblackjr@gmail.com>
2016-12-25 04:58:53 -0500

    Began styling setlist form

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,6 +1,8 @@
 class Medium < ActiveRecord::Base
 
 	belongs_to :user
+	belongs_to :setlist
+
 	mount_uploader :upload, MediauploadUploader
 	mount_uploader :videoupload, MediauploadUploader#VideouploaderUploader
 	mount_uploader :audioupload, AudioUploader
